6de15562fd552acbbea9d7cdf17ef0d1d7608c59
Kontari <Kontari>
2020-05-03 14:41:52 -0400

    update documentation, fixup code

---
+++ README.md
@@ -0,0 +1,10 @@
+# Terminal screensaver program
+Snow is a terminal screensaver written for fun
+
+![Demo](snow_demo.gif)
+
+
+# Running Snow
+```bash
+python Snow.py
+```
--- Snow.py
+++ Snow.py
@@ -1,12 +1,10 @@
+#/usr/bin/env python
+import datetime
 import random as r
-import sys, curses, datetime, time
-'''
-TODO:
-snowman
-clock?
-trees
-debug wind force?
-'''
+import sys
+import time
+
+import curses
 
 tree = """
               v .   ._, |_  .,
@@ -22,111 +20,112 @@ tree = """
                      |    |
 -..----._.---.___---/ ,  . \--._"""
 
-numbers = []
+
+X_SPEED_LIM = 2
+Y_SPEED_LIM = 3
+NUM_SNOWFLAKES = 100
+
 
 class Snow:
-	def __init__(self, max_y, max_x):
-		self.x = r.randint(0,max_x-1)
-		self.y = r.randint(0,max_y-1)
-		self.sprite = r.choice(['*','+',"."])
-		self.speed = r.randint(1,3)
-		self.wind = r.randint(-1,1)
+    def __init__(self, max_y, max_x):
+        self.x = r.randint(X_SPEED_LIM, max_x - X_SPEED_LIM)
+        self.y = r.randint(Y_SPEED_LIM, max_y - Y_SPEED_LIM)
+        self.sprite = r.choice(['*', '+', "."])
+        self.speed = r.randint(1, Y_SPEED_LIM)
+        self.wind = r.randint(-1 * X_SPEED_LIM, X_SPEED_LIM)
 
 
 def main():
 
-	# Configure window
-	stdscr = curses.initscr()
-	curses.noecho()
-	curses.curs_set(0)
+    # Configure window
+    stdscr = curses.initscr()
+    curses.noecho()
+    curses.curs_set(0)
 
-	max_y, max_x = stdscr.getmaxyx()
+    max_y, max_x = stdscr.getmaxyx()
 
-	'''
+    '''
 	Generate a ground pattern resembling snow
 	by alternating patterns of ..__ and ..--
 	'''
-	ground = ""
-	for i in range(0,max_y):
-		ground += (r.randint(1,6)*".")
-		ground += (r.randint(1,4)*r.choice(["-","_"]))
+    ground = ""
+    for i in range(0, max_y):
+        ground += (r.randint(1, 6) * ".")
+        ground += (r.randint(1, 4) * r.choice(["-", "_"]))
 
-	'''
+    '''
 	Generate a backround image. Stored in a list this will be parsed
 	and written behind the snow layer.
 	'''
-	background = []
+    background = []
 
-	for x in range(0,max_x):
-		temp = []
+    for x in range(0, max_x):
+        temp = []
 
-		for y in range(0,max_y):
-			temp.append(" ")
+        for y in range(0, max_y):
+            temp.append(' ')
 
-		background.append(temp)
+        background.append(temp)
 
+    '''
+        Logic to continually draw snowflakes on the screen
+        '''
+    try:
+        snowflakes = []
 
-	try:
-		snowflakes=[]
+        # Generating snowflakes
+        for i in range(0, NUM_SNOWFLAKES):
+            snowflakes.append(Snow(max_y, max_x))
 
-		# Generating snowflakes
-		for i in range(0,100):
-			snowflakes.append(Snow(max_y,max_x))
+        # Draw the background at first
+        for x in range(0, max_x-1):
+            for y in range(0, max_y-1):
+                stdscr.addch(y, x, background[x][y])
 
-		# Draw the background at first
-		for x in range(0,max_x-1):
-			for y in range(0,max_y-1):
-				stdscr.addch(y,x, background[x][y])
-		# Some error here???
+        while True:
+            # Draw the ground
+            stdscr.addnstr(max_y-2, 0, ground * 100, max_x)
 
-		while 1:
-			# Draw the ground
-			stdscr.addnstr(max_y-2,0,ground*100,max_x)
+            # Draw the tree
+            stdscr.addstr(max_y-14, max_x-50, tree)
 
-			# Draw the tree
-			stdscr.addstr(max_y-14, max_x-50, tree)
+            for s in snowflakes:
+                '''
+                Overwrite old flake position with corresponding
+                background x,y character
+                '''
+                stdscr.addch(s.y, s.x, background[s.x][s.y])
 
-			# Need to replace these with an overall back. drawing
+                # Bounds checking for vertical movement
+                if (s.y + s.speed < max_y):
+                    s.y += s.speed
+                else:  # reset flake if out of bounds
+                    s.y = 1
+                    s.x = r.randint(X_SPEED_LIM, max_x - X_SPEED_LIM)
 
-			for s in snowflakes:
+                # Bounds checking for horizontal movement
+                if (2 < s.x + s.wind < max_x-2):
+                    s.x += s.wind
+                else:  # reset flake if out of bounds
+                    s.y = 1
+                    s.x = r.randint(X_SPEED_LIM, max_x - X_SPEED_LIM)
 
-				'''
-				Overwrite old flake position with corresponding
-				background x,y character
-				'''
-				stdscr.addch(s.y, s.x, background[s.x][s.y])
-				#stdscr.addch(s.y, s.x, ' ')
+                # Draw new snowflake position
+                stdscr.addch(s.y, s.x, s.sprite)
 
-				# Bounds checking for vertical movement
-				if (s.y + s.speed < max_y):
-					s.y += s.speed
-				else: # reset flake if out of bounds
-					s.y = 0
-					s.x = r.randint(abs(s.wind),max_x-abs(s.wind))
+            # Draw the time
+            time_raw = datetime.datetime.now().strftime("%I:%M:%S")
+            stdscr.addstr(int(max_y/2), int(max_x/2)-8, str(time_raw))
 
-				# Bounds checking for horizontal movement
-				if ((s.x + s.wind < max_x-2) and (s.x - s.wind > 2)):
-					s.x += s.wind
-				else: # reset flake if out of bounds
-					s.y = 0
-					s.x = r.randint(abs(s.wind),max_x-abs(s.wind))
+            time.sleep(0.2)
 
-				# Draw new snowflake position
-				stdscr.addch(s.y, s.x, s.sprite)
+            stdscr.refresh()
 
-			# Draw the time
-			time_raw = datetime.datetime.now().strftime("%I:%M:%S")
-			stdscr.addstr(int(max_y/2), int(max_x/2)-8, str(time_raw))
+    except KeyboardInterrupt:
+        print("Exiting.")
 
-			# For slower computers this line can be commented out
-			time.sleep(0.1)
+    finally:
+        curses.endwin()
 
-			stdscr.refresh()
-
-	except KeyboardInterrupt:
-		print("Exiting.")
-
-	finally:
-		curses.endwin()
 
 main()
